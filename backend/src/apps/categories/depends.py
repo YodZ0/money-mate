917e1a78115848eb4e1a1aeafc17b74cabd46277
YodZ0 <timur.g00gle@gmail.com>
2025-10-31 17:28:24 +0500

    feat: create categories app dependencies

---
+++ backend/src/apps/categories/depends.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+from fastapi import Depends
+
+from src.core.database import SessionDep
+from .repositories import CategoryRepositoryImpl, CategoryTypeRepositoryImpl
+from .services import CategoryServiceProtocol, CategoryServiceImpl
+
+
+#### REPOSITORIES ####
+def get_category_repository(session: SessionDep) -> CategoryRepositoryImpl:
+    return CategoryRepositoryImpl(session=session)
+
+
+def get_category_type_repository(session: SessionDep) -> CategoryTypeRepositoryImpl:
+    return CategoryTypeRepositoryImpl(session=session)
+
+
+CategoryRepository = Annotated[
+    CategoryRepositoryImpl,
+    Depends(get_category_repository),
+]
+
+CategoryTypeRepository = Annotated[
+    CategoryTypeRepositoryImpl,
+    Depends(get_category_type_repository),
+]
+
+
+#### SERVICES ####
+def get_category_service(repository: CategoryRepository) -> CategoryServiceProtocol:
+    return CategoryServiceImpl(repository)
+
+
+CategoryService = Annotated[
+    CategoryServiceProtocol,
+    Depends(get_category_service),
+]
