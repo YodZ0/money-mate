5dd9eff3b1b6543627beeea7e567ca0d580d9c7e
YodZ0 <timur.g00gle@gmail.com>
2025-10-31 17:11:07 +0500

    feat: create db enum

---
+++ backend/src/core/enums.py
@@ -0,0 +1,35 @@
+from typing import Self
+from enum import StrEnum, auto
+
+
+class CascadeEnum(StrEnum):
+    """
+    Настройки каскадного поведения для SQLAlchemy.
+    """
+
+    SAVE_UPDATE = "save-update"
+    MERGE = "merge"
+    REFRESH_EXPIRE = "refresh-expire"
+    EXPUNGE = "expunge"
+    DELETE = "delete"
+    ALL = "all"
+    DELETE_ORPHAN = "delete-orphan"
+
+    ALL_DELETE_ORPHAN = "all, delete-orphan"
+
+    def __add__(self: Self, value: str) -> str:
+        return f"{self}, {value}"
+
+    def __radd__(self: Self, value: str) -> str:
+        return f"{value}, {self}"
+
+
+class ModelActionEnum(StrEnum):
+    """
+    Действие с моделью.
+    """
+
+    INSERT = auto()
+    UPDATE = auto()
+    UPSERT = auto()
+    DELETE = auto()
