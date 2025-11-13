3937a490a715f331f5f74b809952e11193c4c1f5
YodZ0 <timur.g00gle@gmail.com>
2025-11-13 22:12:57 +0500

    tests: update models creation + add crud_parent_repository fixture

--- backend/tests/repositories/crud/conftest.py
+++ backend/tests/repositories/crud/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import AsyncGenerator
 
 from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,12 +10,20 @@ from src.core.models.base import Base
 
 from tests.settings import settings
 
-from .crud import CrudTestRepository
-from .models import CrudTestModel
-from .schemas import CrudTestCreateSchema
+from .crud import CrudParentTestRepository, CrudTestRepository
+from .models import CrudParentTestModel, CrudTestModel
+from .schemas import CrudParentTestCreateSchema, CrudTestCreateSchema
 
 
-MODELS_TO_CREATE = [CrudTestCreateSchema(label=f"Test model {i}") for i in range(1, 11)]
+MODELS_TO_CREATE = [
+    CrudTestCreateSchema.model_validate(
+        {
+            "label": f"Test model {i}",
+            "parentId": 1,
+        }
+    )
+    for i in range(1, 11)
+]
 
 
 async def create_models(
@@ -24,6 +33,10 @@ async def create_models(
     """
     Создаем тестовые данные в БД.
     """
+    stmt = insert(CrudParentTestModel).values(
+        **CrudParentTestCreateSchema(name="Parent 1").model_dump(exclude={"id"})
+    )
+    await session.execute(stmt)
     for model in models_to_create:
         stmt = insert(CrudTestModel).values(**model.model_dump(exclude={"id"}))
         await session.execute(stmt)
@@ -31,6 +44,9 @@ async def create_models(
 
 @pytest.fixture(scope="function", autouse=True)
 async def prepare_db():
+    """
+    Создаем таблицы в БД, наполняем данными, очищаем до и после каждого теста.
+    """
     db_provider = DatabaseProvider(settings.db.dsn)
     async with db_provider.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -45,7 +61,20 @@ async def prepare_db():
 
 
 @pytest.fixture
-async def crud_repository():
+async def crud_repository() -> AsyncGenerator[CrudTestRepository]:
+    """
+    Репозиторий для модели CrudTestModel.
+    """
     db_provider = DatabaseProvider(settings.db.dsn)
     async with db_provider.session_factory() as session:
         yield CrudTestRepository(SessionManagerImpl(session))  # type: ignore
+
+
+@pytest.fixture
+async def crud_parent_repository() -> AsyncGenerator[CrudParentTestRepository]:
+    """
+    Репозиторий для модели CrudParentTestModel.
+    """
+    db_provider = DatabaseProvider(settings.db.dsn)
+    async with db_provider.session_factory() as session:
+        yield CrudParentTestRepository(SessionManagerImpl(session))  # type: ignore
