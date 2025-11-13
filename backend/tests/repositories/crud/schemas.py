3305ff3aae888ac22843ef6e057bd4724b9409de
YodZ0 <timur.g00gle@gmail.com>
2025-11-13 22:10:33 +0500

    tests: add CurdParentTestModel schemas + add parent_id for CrudTestModel schemas

--- backend/tests/repositories/crud/schemas.py
+++ backend/tests/repositories/crud/schemas.py
@@ -8,13 +8,28 @@ from src.core.schemas.repository import (
 from src.core.schemas.request_response import RequestSchema, ResponseSchema
 
 
+class CrudParentTestReadSchema(ResponseSchema, ReadSchemaInt):
+    name: str
+
+
+class CrudParentTestCreateSchema(RequestSchema, CreateSchemaInt):
+    name: str
+
+
+class CrudParentTestUpdateSchema(RequestSchema, UpdateSchemaInt):
+    name: Optional[str] = None
+
+
 class CrudTestReadSchema(ResponseSchema, ReadSchemaInt):
     label: str
+    parent_id: int
 
 
 class CrudTestCreateSchema(RequestSchema, CreateSchemaInt):
     label: str
+    parent_id: int
 
 
 class CrudTestUpdateSchema(RequestSchema, UpdateSchemaInt):
     label: Optional[str] = None
+    parent_id: Optional[int] = None
